6b83711b23cc0118d796b0e40b2fe8f9de2c8ee1
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-07-30 08:27:51 +0100

    pull

--- Municipality/App.js
+++ Municipality/App.js
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from 'react'
 import { firebase } from './src/firebase/config'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+import { LoginScreen, HomeScreen, RegistrationScreen, ParametreScreen, ComplainScreen } from './src/screens';
 import {decode, encode} from 'base-64'
+
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
@@ -53,8 +54,11 @@ export default function App() {
           <>
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Registration" component={RegistrationScreen} />
-          </>
+          </>  
         )}
+          <Stack.Screen name="Complain" component={ComplainScreen} />
+          <Stack.Screen name="Parametre" component={ParametreScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
