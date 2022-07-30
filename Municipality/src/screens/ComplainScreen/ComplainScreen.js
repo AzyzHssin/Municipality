94d78faaa082ccccc9de149d9937f0df8da3cbc5
Azyz hssin <hssine.medaizz1@gmail.com>
2022-07-30 14:26:54 +0100

    workin on structure

--- Municipality/src/screens/ComplainScreen/ComplainScreen.js
+++ Municipality/src/screens/ComplainScreen/ComplainScreen.js
@@ -1,13 +1,35 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Appbar } from 'react-native-paper';
+import  { useState } from 'react'
+import {firebase} from "../../firebase/config"
+import SelectDropdown from 'react-native-select-dropdown'
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import styles from './styles.js'
+export default function ComplainScreen({ navigation }) {
 
-export default function DetailsScreen({ navigation }) {
+
+    const[topic,settopic]=useState('')
+    const [body,setbody]=useState('')
+    const [image,setimage]=useState('')
+    const FireBaseRef=firebase.firestore().collection('Complains')
+    const [page,setPage]=useState(1)
+
+    var handlesubmit= async ()=>{
+        await  FireBaseRef.add({topic:topic,body:body,image:image})
+        alert("added successfully")
+      }
+      
+       
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Appbar.Header>
+            <Appbar.Content title="Complains" />
+            </Appbar.Header>
             <Text
                 onPress={() => navigation.navigate('Home')}
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Complains Screen</Text>
-                
-        </View>
+<View style={styles.container}   >    </View>
+</View>
     );
 }
