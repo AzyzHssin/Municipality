de041bf1e0c4038618de441159d19393c20acce2
Azyz hssin <hssine.medaizz1@gmail.com>
2022-07-30 15:39:39 +0100

    styling

--- Municipality/src/screens/ComplainScreen/ComplainScreen.js
+++ Municipality/src/screens/ComplainScreen/ComplainScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAvoidingView,ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { Appbar } from 'react-native-paper';
 import  { useState } from 'react'
 import {firebase} from "../../firebase/config"
@@ -7,7 +7,7 @@ import SelectDropdown from 'react-native-select-dropdown'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import styles from './styles.js'
 export default function ComplainScreen({ navigation }) {
-
+    const bg1 = { uri: "https://images.unsplash.com/photo-1503596476-1c12a8ba09a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" };
 
     const[topic,settopic]=useState('')
     const [body,setbody]=useState('')
@@ -22,14 +22,13 @@ export default function ComplainScreen({ navigation }) {
       
        
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Appbar.Header>
-            <Appbar.Content title="Complains" />
+        <View >
+            <Appbar.Header styles={styles.header}>
+            <Appbar.Content title="Complains" onPress={() => navigation.navigate('Home')}/>
             </Appbar.Header>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Complains Screen</Text>
-<View style={styles.container}   >    </View>
+<View style={styles.complainContainer}   > <ImageBackground source={bg1} resizeMode="cover" style={{width: '100%', height: '100%',flex: 1,justifyContent: "center"}}></ImageBackground>   </View>
+<View style={styles.complainContainer}   >    </View>
+<View style={styles.complainContainer}   >    </View>
 </View>
     );
 }
