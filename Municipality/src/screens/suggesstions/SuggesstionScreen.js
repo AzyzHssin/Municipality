15e69e31f65dc84512672f55491da053215efdb3
haythembensouissi <haythembensouissi2@gmail.com>
2022-07-31 08:45:29 +0100

    working on the sugussetions

--- Municipality/src/screens/suggesstions/SuggesstionScreen.js
+++ Municipality/src/screens/suggesstions/SuggesstionScreen.js
@@ -15,19 +15,8 @@ const ref=firebase.firestore().collection('suggestions')
 var handlesubmit= async ()=>{
   await  ref.add({type:type,municipalityname:name,description:desc})
   alert("added successfully")
+  
 }
-    var handlepressgeneral=()=>{
-ref.add({type:"general"})
-        navigation.navigate("Municipality")
-    }
-    var handlepresselectricity=()=>{
-        ref.add({type:"garbage"})
-                navigation.navigate("Municipality")
-            }
-            var handlepressgarbage=()=>{
-                ref.add({type:"garbage"})
-                        navigation.navigate("Municipality")
-                    }
   return (
     <KeyboardAvoidingView style={styles.container} behavior='padding'>
     <View style={styles.inputcontainer}>
@@ -50,52 +39,25 @@ ref.add({type:"general"})
             return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#444'} size={18} />;
           }}
           defaultButtonText={'Select type '}
-    />:page===2 ?     <TextInput style={styles.input} value={name} onChangeText={setname} placeholder='municipality name ' />
-    : page===3?(    <TextInput style={styles.input} value={desc} onChangeText={setdesc} placeholder='description ' />
+    />:page===2 ?<TextInput style={styles.input} value={name} onChangeText={setname} placeholder='municipality name ' />
+    : page===3?(<TextInput style={styles.input} value={desc} onChangeText={setdesc} placeholder='description ' />
     ):null}
-
-    {page<3?(
-        <TouchableOpacity onPress={()=>{
+   {/* {page<3?(
+        <View style={styles.button}>
+        <TouchableOpacity style={styles.but} onPress={()=>{
         const nextpage=page+1
         setPage(nextpage)
-    }}><Text>next</Text></TouchableOpacity>):null}
-
-   {/* <SelectDropdown
-	data={options}
-	onSelect={(selectedItem, index) => {
-		console.log(selectedItem, index)
-        setType(selectedItem)
-
-	}}
-	buttonTextAfterSelection={(selectedItem, index) => {
-		
-		return selectedItem
-	}}
-	rowTextForSelection={(item, index) => {
-		
-		return item
-	}}
-    renderDropdownIcon={isOpened => {
-        return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#444'} size={18} />;
-      }}
-      defaultButtonText={'Select type '}
-    />*/}
-    {/* <TextInput style={styles.input} value={name} onChangeText={setname} placeholder='municipality name ' />*/}
-   {/* <TextInput style={styles.input} value={desc} onChangeText={setdesc} placeholder='description ' />*/}
-   {/*  <TouchableOpacity onPress={()=>handlepressgeneral()}><Text>general</Text></TouchableOpacity>
-     <TouchableOpacity onPress={()=>handlepresselectricity()} ><Text>electricity</Text></TouchableOpacity>
-     <TouchableOpacity onPress={()=>handlepressgarbage()}><Text>garbage</Text></TouchableOpacity>*/}
-
- {page ===3? (<View style={styles.button}><TouchableOpacity onPress={()=>handlesubmit()}><Text>submit</Text></TouchableOpacity></View>):null} 
+    }}><Text style={styles.buttontext}>next</Text></TouchableOpacity></View>):null} */}
+ {/*{page ===3? (<View style={styles.button}><TouchableOpacity style={styles.but} onPress={()=>handlesubmit()}><Text style={styles.buttontext}>submit</Text></TouchableOpacity></View>):null} */}
   
- {/* {page > 1 ? (<View>
-    <TouchableOpacity onPress={()=>{
-        const prevpage=page-1 
-        setPage(prevpage)
-    }}><Text>back</Text></TouchableOpacity>
-    </View>):null}
-*/}
-{page>1?(<TouchableOpacity onPress={()=>{setPage(page-1)}}><Text>back</Text></TouchableOpacity>):null}
+{/*{page>1?(<TouchableOpacity onPress={()=>{setPage(page-1)}}><Text>back</Text></TouchableOpacity>):null}*/}
+
+
+    </View>
+    <View style={styles.buttoncontainer}>
+    {page<3?(<View style={styles.button}><TouchableOpacity style={styles.but} onPress={()=>setPage(page+1)}><Text>next</Text></TouchableOpacity></View>):null}
+    {page>1?(<View style={styles.button}><TouchableOpacity style={styles.but} onPress={()=>setPage(page-1)}><Text>back</Text></TouchableOpacity></View>):null}
+    {page ===3? (<View style={styles.button}><TouchableOpacity style={styles.but} onPress={()=>handlesubmit()}><Text style={styles.buttontext}>submit</Text></TouchableOpacity></View>):null} 
 
     </View>
     </KeyboardAvoidingView>
